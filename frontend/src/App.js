1bbf9a84d6c321faccc6e45bd7773533215d9576
Matthew Anderson <matt.anderson.inf@gmail.com>
2023-07-18 01:12:20 -0400

    reorganize App.js, Routes for Login and SignUp work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,34 @@
 /* code block 2 */
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import LogIn from './pages/LogIn';
 import SignUp from './pages/Signup';
 import Personal from './pages/Personal';
 import Navbar from './components/Navbar';
 
-const App = () => {
-  const [content, setContent] = useState('home');
-
-  const handleButtonClick = (page) => {
-    setContent(page);
-  };
-
-  let pageContent;
-
-  switch (content) {
-    case 'home':
-      pageContent = <Home />;
-      break;
-    case 'about': 
-      pageContent = <About />;
-      break;
-    case 'login':
-      pageContent = <LogIn />;
-      break;
-    case 'signup':
-      pageContent = <SignUp />;
-      break;
-    case 'personal':
-      pageContent = <Personal />;
-      break;
-    default:
-      pageContent = <Home />;
-  }
-
+function App () {
   return (
     <BrowserRouter>
       <div className="container">
         <Navbar/>
-        <div className="sidebar">
-            <button className="button" onClick={() => handleButtonClick('home')}>Home</button>
-            <button className="button" onClick={() => handleButtonClick('about')}>About</button>
-            <button className="button" onClick={() => handleButtonClick('signin')}>Sign In</button>
-            <button className="button" onClick={() => handleButtonClick('personal')}>Personal</button>
-        </div>
         <h1>REILib</h1>
         <Routes>
           <Route
+            path = "/"
+            element= {<Home/>}
+          />
+          <Route
             path="/login"
-            element={pageContent}
-          >
-          </Route>
+            element={<LogIn/>}
+          />
+          <Route
+            path="/signup"
+            element={<SignUp/>}
+          />
+          <Route
+            path="/personal"
+            element={<Personal/>}
+          />
         </Routes>
       </div>
     </BrowserRouter>
