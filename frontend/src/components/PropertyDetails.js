ebe778f106b64985568593c87ee0e9a970d3ad80
Matthew Anderson <matt.anderson.inf@gmail.com>
2024-04-21 21:20:15 -0400

    navigation between home page and property page alters property in local storage properly

--- frontend/src/components/PropertyDetails.js
+++ frontend/src/components/PropertyDetails.js
@@ -1,3 +1,9 @@
+/* PropertyDetails.js
+
+component to view the detils of a given property
+
+*/
+
 
 import { usePropertiesContext } from "../hooks/usePropertiesContext";
 import { useAuthContext } from "../hooks/useAuthContext";
@@ -6,7 +12,7 @@ import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import { Link } from "react-router-dom"; 
 
 
-/*
+
 function reformatDate(dateString) {
     let date = new Date(dateString); 
     let hours = date.getHours().toString().padStart(2, '0');
@@ -18,7 +24,6 @@ function reformatDate(dateString) {
     
     return `${hours}:${minutes}:${seconds}, ${month}/${day}/${year}`;
 } 
-*/ 
 
 
 const PropertyDetails = ({property}) => {
@@ -27,39 +32,9 @@ const PropertyDetails = ({property}) => {
     const { dispatch } = usePropertiesContext();
 
     const { user } = useAuthContext();
-
-    
-    // console.log("User AuthContext in PropertyDetails: ", user)
     
 
-    const deleteProperty=  async() => {
-
-        
-        if(!user){
-            return
-        }
-
-
-        const response = await fetch('/api/properties/' + property._id, 
-            {
-                method: 'DELETE',
-                headers: {
-                    'Authorization': `Bearer ${user.token}`
-                }
-        
-            }
-        )
-
-        const json = await response.json()
-
-        if (response.ok){
-
-            dispatch({type:'DELETE_PROPERTY', payload: json})
-
-        }
-
-
-    }
+    
 
     // get the 
     const routeToPropertyPage = async() =>{
@@ -67,8 +42,6 @@ const PropertyDetails = ({property}) => {
         if(!user){
             return
         }
-
-        console.log("route to property firing with this id:", property._id)
 
 
         const response = await fetch('/api/properties/' + property._id, 
@@ -87,11 +60,14 @@ const PropertyDetails = ({property}) => {
 
         if (response.ok){
 
-            // navigate("/properties"+property._id)
-
-            console.log("thisis the json respone",json)
+            if (localStorage.getItem("property")){
+                console.log("attempting to remove localstorage property")
+                localStorage.removeItem("property");
+            }
 
             localStorage.setItem('property',JSON.stringify(json))
+
+            window.location.href = "/properties/"+property._id;
 
         }
 
@@ -100,9 +76,7 @@ const PropertyDetails = ({property}) => {
     //href={"/properties/"+property._id}
 
     return (
-        <div className="property-details" id={property._id}>
-
-            <Link to={"/properties/"+property._id} id={property._id} onClick={routeToPropertyPage}>
+        <div className="property-details" id={property._id} onClick={routeToPropertyPage}>
                 <h4>{
                     property.StreetNumber+' '+property.StreetName+', '+property.City+', '+property.StateOrProvince+', '+property.PostalCode
                 }</h4>
@@ -123,9 +97,8 @@ const PropertyDetails = ({property}) => {
                 }</p>
                 <p>{formatDistanceToNow(new Date(property.createdAt), {addSuffix:true} )}</p>
                 {
-                    //{reformatDate(property.createdAt)}
+                    reformatDate(property.createdAt)
                 }
-            </Link>
             
             {
                 // <span className="material-symbols-outlined" onClick={deleteProperty}>Delete</span>
--- frontend/src/pages/PropertyPage.js
+++ frontend/src/pages/PropertyPage.js
@@ -1,15 +1,84 @@
-import {useState} from "react";
+/* PropertyPage.js 
+
+webpage to view and edit information about a proprerty
+
+*/
+
+// import {useState} from "react";
+import { usePropertiesContext } from "../hooks/usePropertiesContext";
+import { useAuthContext } from "../hooks/useAuthContext";
+// import { Link } from "react-router-dom";
 
 const PropertyPage = ({ children }) => {
 
+    const { dispatch } = usePropertiesContext();
+
+    const { user } = useAuthContext();
+
+    const deleteProperty=  async() => {
+
+        
+        if(!user){
+            return
+        }
+
+
+        const response = await fetch('/api/properties/' + property._id, 
+            {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+        
+            }
+        )
+
+        const json = await response.json()
+
+        if (response.ok){
+
+            dispatch({type:'DELETE_PROPERTY', payload: json})
+
+            if (localStorage.getItem("property")){
+
+                localStorage.removeItem("property");
+            
+            }
+
+
+        }
+
+        window.location.href = "/";
+
+
+    }
+
+    const property = JSON.parse(localStorage.getItem("property"))
+
+    console.log("property:", property)
   
     return (
     <div>
-        <h4>test</h4>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
+        <h4>{
+            property.StreetNumber+' '+property.StreetName+', '+property.City+', '+property.StateOrProvince+', '+property.PostalCode
+        }</h4>
+        <p><strong>Address: </strong>{
+            property.StreetNumber+' '+property.StreetName+', '+property.City+', '+property.StateOrProvince+', '+property.PostalCode
+        }</p>
+        <p><strong>List Price: </strong>{
+            property.ListPrice
+        }</p>
+        <p><strong>Living Area: </strong>{
+            property.LivingArea
+        }</p>
+        <p><strong>Total Bedrooms: </strong>{
+            property.BedroomsTotal
+        }</p>
+        <p><strong>Total Bathrooms: </strong>{
+            property.BathroomsTotalDecimal
+        }</p>
+        <button onClick={deleteProperty} className="delete">Delete</button>
+   
     </div>
     );
   };
